527b74882b353a3b49249960295f57c9ed375ef6
Ivánek <Ivnek@Zalman_Z9>
2020-04-17 12:05:57 +0200

    .

---
+++ test.php
@@ -0,0 +1,126 @@
+ <?PHP
+   echo 420;
+echo"<br>";
+echo"<br>";
+echo"<br>";
+print (69.5);
+echo"<bambus>";
+echo"<br>";
+echo"<br>";
+echo"<br>";
+print (FALSE);
+echo" slong "." dlouhy ";
+echo "<img src=\"x.jpg\" alt=\"neco\">";
+echo"\\";
+
+            $a = 69;
+            echo"<br>";
+            $b = 420;
+
+            echo "<br>";
+
+            var_dump($a + $b);
+
+            echo "<br>";
+
+            $a = 69;
+            echo"<br>";
+            $b = 420;
+
+            var_dump($a - $b);
+
+            echo "<br>";
+
+            $a = 69;
+            echo"<br>";
+            $b = 420;
+
+            var_dump($a * $b);
+
+            echo "<br>";
+
+            $a = 69;
+            echo"<br>";
+            $b = 240;
+
+            var_dump($a / $b);
+
+            echo "<br>";
+
+            $a = 69;
+            echo"<br>";
+            $b = 420;
+
+            var_dump($a += $b);
+
+            echo "<br>";
+
+            $a = 69;
+            echo"<br>";
+            $b = 420;
+
+            var_dump($a -= $b);
+
+            echo "<br>";
+
+            $a = 69;
+            echo"<br>";
+            $b = 420;
+
+            var_dump($a *= $b);
+
+            echo "<br>";
+
+            $a = 69;
+            echo"<br>";
+            $b = 420;
+
+            var_dump($a /= $b);
+
+            echo "<br>";
+
+            $a = 69;
+            echo"<br>";
+            $b = 420;
+
+            var_dump($a == $b);
+
+            echo "<br>";
+
+            $a = 69;
+            $b = 420;
+
+            var_dump($a != $b);
+
+            echo "<br>";
+
+            $a = 69;
+            echo"<br>";
+            $b = 420;
+
+            var_dump($a > $b);
+
+            echo "<br>";
+            echo"<br>";
+
+            $a = 69;
+            echo"<br>";
+            $b = 420;
+
+            var_dump($a < $b);
+
+            echo "<br>";
+            $a = 69;
+            echo"<br>";
+            $b = 420;
+
+            var_dump($a || $b);
+
+            echo "<br>";
+            $a = 69;
+            echo"<br>";
+            $b = 420;
+
+            var_dump($a & $b);
+ 
+   ?>
